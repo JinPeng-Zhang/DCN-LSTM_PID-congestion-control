4ac3bb79f110cb58c5d25311bfdcde2e2dff6738
ZJPZJY <2530064451@qq.com>
2023-04-08 22:22:46 +0800

    ...

--- ns3-simulate/PIDNN/pt_reader.py
+++ ns3-simulate/PIDNN/pt_reader.py
@@ -12,17 +12,29 @@ bh = x.lstm.bias_hh_l0
 linearw = x.linear0.weight
 linearb = x.linear0.bias
 
-
 h_0 = torch.zeros((1, 1, 16))
 c_0 = torch.zeros((1, 1, 16))
-x1 = torch.tensor([1.]).reshape((1, 1, 1))
 
+xs = torch.tensor([0.151861,0.284071,0.362368]).reshape((1, 3, 1))
+x1 = xs[0][0]
 it = torch.sigmoid((wi[0:16]*x1).reshape((16,1))+bi[0:16].reshape((16,1))+torch.mm(wh[0:16],h_0[0].T)+bh[0:16].reshape((16,1)))
 fi = torch.sigmoid((wi[16:32]*x1).reshape((16,1))+bi[16:32].reshape((16,1))+torch.mm(wh[16:32],h_0[0].T)+bh[16:32].reshape((16,1)))
 gt = torch.tanh((wi[32:48]*x1).reshape((16,1))+bi[32:48].reshape((16,1))+torch.mm(wh[32:48],h_0[0].T)+bh[32:48].reshape((16,1)))
 ot = torch.sigmoid((wi[48:64]*x1).reshape((16,1))+bi[48:64].reshape((16,1))+torch.mm(wh[48:64],h_0[0].T)+bh[48:64].reshape((16,1)))
-ct = fi*c_0.reshape((16,1))+it*gt
-ht = ot*torch.tanh(ct)
-
-print(ct,ht)
-print(x.lstm(x1,(h_0,c_0)))
+c_0 = (fi*c_0.reshape((16,1))+it*gt)
+h_0 = (ot*torch.tanh(c_0))
+c_0 = c_0.reshape((1, 1, 16))
+h_0 = h_0.reshape((1, 1, 16))
+x1 = xs[0][1]
+it = torch.sigmoid((wi[0:16]*x1).reshape((16,1))+bi[0:16].reshape((16,1))+torch.mm(wh[0:16],h_0[0].T)+bh[0:16].reshape((16,1)))
+fi = torch.sigmoid((wi[16:32]*x1).reshape((16,1))+bi[16:32].reshape((16,1))+torch.mm(wh[16:32],h_0[0].T)+bh[16:32].reshape((16,1)))
+gt = torch.tanh((wi[32:48]*x1).reshape((16,1))+bi[32:48].reshape((16,1))+torch.mm(wh[32:48],h_0[0].T)+bh[32:48].reshape((16,1)))
+ot = torch.sigmoid((wi[48:64]*x1).reshape((16,1))+bi[48:64].reshape((16,1))+torch.mm(wh[48:64],h_0[0].T)+bh[48:64].reshape((16,1)))
+c_0 = fi*c_0.reshape((16,1))+it*gt
+h_0 = ot*torch.tanh(c_0)
+# print(h_0)
+#
+# h_0 = torch.zeros((1, 1, 16))
+# c_0 = torch.zeros((1, 1, 16))
+print(x(xs))
+#print(linearw,linearb)
--- ns3-simulate/rtt平滑预测.py
+++ ns3-simulate/rtt平滑预测.py
@@ -3,10 +3,10 @@ from model.rtt_pred import models
 from torch import nn,optim
 import random
 import matplotlib.pyplot as plt
-file = open("PIDNN/0.txt")
+file = open("PIDNN/target5us.txt")
 line = file.readline()
-#model = torch.load("pt2/four.pt")
-model = models()
+model = torch.load("PIDNN/pid_5us.pt")
+#model = models()
 input = []
 ouput = []
 while line:
@@ -19,26 +19,26 @@ loss_fn = nn.L1Loss()
 optimizer = optim.Adam(model.parameters(),lr=1*1e-3)
 print((len(input)))
 for epcoh in range(19):
-
-    los = []
+    #
+    # los = []
     outs = []
-    pre = []
-    for i in range(800):
-        index = random.randint(0,len(input)-1)
-        x = torch.tensor(input[index]).reshape((1, 3, 1))
-        y = torch.tensor(ouput[index]).reshape((1, 1))
-        out = model(x)
-        loss = loss_fn(out, y)  # 计算每一个网络的损失值
-        optimizer.zero_grad()  # 在下一次求导之前将保留的grad清空
-        loss.backward()  # 反向传播，计算梯度
-        optimizer.step()  # 应用求导到优化器上去
-        los.append(float(loss))
-        pre.append(float(abs((out - y)/(1+y))))
-        outs.append(float(out))
-        input.pop(index)
-        ouput.pop(index)
-    print(epcoh)
-    print(torch.mean(torch.tensor(pre)),torch.mean((torch.tensor(los))))
+    # pre = []
+    # for i in range(800):
+    #     index = random.randint(0,len(input)-1)
+    #     x = torch.tensor(input[index]).reshape((1, 3, 1))
+    #     y = torch.tensor(ouput[index]).reshape((1, 1))
+    #     out = model(x)
+    #     loss = loss_fn(out, y)  # 计算每一个网络的损失值
+    #     optimizer.zero_grad()  # 在下一次求导之前将保留的grad清空
+    #     loss.backward()  # 反向传播，计算梯度
+    #     optimizer.step()  # 应用求导到优化器上去
+    #     los.append(float(loss))
+    #     pre.append(float(abs((out - y)/(1+y))))
+    #     outs.append(float(out))
+    #     input.pop(index)
+    #     ouput.pop(index)
+    # print(epcoh)
+    # print(torch.mean(torch.tensor(pre)),torch.mean((torch.tensor(los))))
     pre = []
     los = []
     for i in range(200):
@@ -52,5 +52,6 @@ for epcoh in range(19):
         pre.append(float(abs((out - y)/(1+y))))
         input.pop(index)
         ouput.pop(index)
+        print(out.tolist(),y.tolist(),pre[-1])
     print(torch.mean(torch.tensor(pre)),torch.mean((torch.tensor(los))))
-torch.save(model, "PIDNN/pid_5us.pt")
+#torch.save(model, "PIDNN/pid_5us.pt")
