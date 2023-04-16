c8959269281cac2d4e8cad261c841fbb06295dfe
ZJPZJY <2530064451@qq.com>
2023-04-16 11:44:02 +0800

    ...

--- ns3-simulate/PIDNN/DATA_read/data_reader.py
+++ ns3-simulate/PIDNN/DATA_read/data_reader.py
@@ -3,7 +3,7 @@ import  matplotlib
 import torch
 matplotlib.rcParams['axes.unicode_minus']=False
 plt.rcParams["font.sans-serif"]=["SimHei"]
-file = open("../../LINUX-NS3-SIMULATE/pid_rtt9us.txt")
+file = open("../../LINUX-NS3-SIMULATE/pid_rtt_pred5us.txt")
 
 line = file.readline()
 nod = []
@@ -11,10 +11,9 @@ rate = []
 rtt = []
 while line:
     node,_,_,_,rat,rt,_,_ = line.split(" ")
-    if float(node.split(":")[1])==2:
-        nod.append(float(node.split(":")[1]))
-        rate.append(float(rat.split(":")[1]))
-        rtt.append(float(rt.split("t")[-1]))
+    nod.append(float(node.split(":")[1]))
+    rate.append(float(rat.split(":")[1]))
+    rtt.append(float(rt.split("t")[-1]))
     line = file.readline()
 file.close()
 print(torch.mean(torch.tensor(rtt)),torch.mean(torch.tensor(rate)),max(rtt))
